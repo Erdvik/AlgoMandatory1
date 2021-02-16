9bfcfd659c4396b6e7aac3110770e7c77c76c0ee
Erdvik97 <magnuserdvik@hotmail.com>
2021-02-16 20:45:16 +0100

    la til noe

---
+++ mand1/Editor3/src/editor/Document.java
@@ -0,0 +1,177 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package editor;
+
+import editor.display.CharacterDisplay;
+//import sun.awt.image.ImageWatched;
+
+import java.lang.ProcessBuilder.Redirect.Type;
+import java.util.*;
+
+/**
+ * This class represents the document being edited. Using a 2d array to hold the
+ * document content is probably not a very good choice. Fixing this class is the
+ * main focus of the exercise. In addition to designing a better data model, you
+ * must add methods to do at least basic editing: write and delete text, and
+ * moving the cursor
+ *
+ * @author evenal
+ */
+public class Document {
+
+    private CharacterDisplay display;
+    private int cursorRow;
+    private int cursorCol;
+
+    LinkedList<Character> cursorC = new LinkedList<>();
+    LinkedList<Character> cursorR = new LinkedList<>();
+
+    public Document(CharacterDisplay display) {
+        this.display = display;
+        cursorCol = cursorRow = 0;
+
+   ///    Fill the Linked List with ' ' to avoid getting IndexOutOfBoundsException while traversing
+        int i = 0;
+        int j = 0;
+         while (i <= display.getWidth()) {
+            cursorR.add(' ');
+            i++;
+          }
+
+          while (j <= 800) {
+              cursorC.add(' ');
+              j++;
+          }
+    }
+
+    public void shiftChars() {
+
+        int colIndex = cursorCol;
+        int rowIndex = cursorRow;
+
+        while(colIndex < cursorC.size() -1) {
+
+            Character val = cursorC.get(colIndex + 1);
+            cursorC.add(colIndex + 2, val);
+
+            cursorC.removeLast();
+
+            colIndex++;
+        }
+
+        while(rowIndex < cursorR.size() -1) {
+
+            Character val = cursorR.get(rowIndex + 1);
+            cursorR.add(rowIndex + 2, val);
+
+            cursorR.removeLast();
+
+            rowIndex++;
+        }
+    }
+
+    public void insertChar(char c) {
+      // cursorR.add(cursorRow, c);
+      // cursorC.add(cursorCol, c);
+
+       // shiftChars();
+
+        display.displayChar(c, cursorRow, cursorCol);
+        display.displayCursor(' ', cursorRow, cursorCol);
+
+        if (cursorCol == 39 && cursorRow == 19) {
+        } else {
+            cursorCol++;
+            if (cursorCol >= CharacterDisplay.WIDTH) {
+                cursorCol = 0;
+                cursorRow++;
+            }
+        }
+    }
+
+
+    public void removeChar(char c) {
+       
+        cursorR.add(cursorRow, ' ');
+        cursorC.add(cursorCol, ' ');
+        
+
+        display.displayChar(' ', cursorRow, cursorCol);
+
+        if (cursorCol == 0 && cursorRow == 0) {
+
+        } else if (cursorCol == 0 && cursorRow >= 0) {
+            cursorCol = 39;
+            cursorRow--;
+        } else {
+            cursorCol--;
+        }
+        display.displayCursor(' ', cursorRow, cursorCol);
+    }
+
+    public void shiftChar(char c) {
+        cursorC.add(cursorCol, c);
+        cursorR.add(cursorRow, c);
+
+        display.displayChar(c, cursorRow, cursorCol);
+        if (cursorRow < 19) {
+            int ant = CharacterDisplay.WIDTH - cursorCol;
+            System.out.println(ant);
+            for (int i = 0; i < ant; i++) {
+                cursorC.add('c');
+            }
+            System.out.println(cursorC.size());
+            cursorCol = 0;
+            cursorRow++;
+        }
+        display.displayCursor(' ', cursorRow, cursorCol);
+    }
+
+    public void moveCursorUp() {
+        if (cursorRow == 0) {
+
+        } else if (cursorRow > 0) {
+            cursorRow--;
+        }
+        display.displayCursor(' ', cursorRow, cursorCol);
+    }
+
+    public void moveCursorDown() {
+        if (cursorCol == 19) {
+
+        } else if (cursorRow < 19) {
+            cursorRow++;
+        }
+        display.displayCursor(' ', cursorRow, cursorCol);
+    }
+
+    public void moveCursorRight() {
+
+        if (cursorCol == 39 && cursorRow == 19) {
+        } else {
+            cursorCol++;
+            if (cursorCol >= CharacterDisplay.WIDTH) {
+                cursorCol = 0;
+                cursorRow++;
+            }
+        }
+
+        display.displayCursor(' ', cursorRow, cursorCol);
+    }
+
+    public void moveCursorLeft() {
+        if (cursorCol == 0 && cursorRow == 0) {
+
+        } else if (cursorCol == 0 && cursorRow >= 0) {
+            cursorCol = 39;
+            cursorRow--;
+        } else {
+            cursorCol--;
+        }
+        display.displayCursor(' ', cursorRow, cursorCol);
+    }
+}
+
---
+++ mand1/README.md
@@ -0,0 +1,31 @@
+# Algoritmer og datastrukturer oppgave 1
+Av Martin Ruud og Kristian Bjurholt Rein 
+
+<h4> Keybindings </h4>
+
+"A-Z|0-1|a-z|§-?|Å-Ø|å-ø|" = karakter innsetting
+
+"Backspace/Delete" = sletting
+ 
+"Enter" = linjeskifte
+
+"Space" = mellomrom
+
+"ALT" + "←" = beveg cursor til venstre
+
+"ALT" + "→" = beveg cursor til høyre
+
+"ALT" + "↑" = beveg cursor oppover
+
+"ALT" + "↓" = beveg cursor nedover
+
+Programmet støtter alle karakterer mellom kontrollkarakter for backspace til liten ø i unicode.
+
+
+
+
+<h4> Beskrivelse av datastrukturer </h4> 
+I denne oppgaven valgte vi å bruke Linked List som datastruktur i Document klassen. Grunnen til dette er at en Linked List består av noder som peker på hverandre. Om man da velger å slette en karakter, vil bare pekeren til neste node endres. Etter mye prøving og feiling har vi til slutt kommet frem til en Linked List som inneholder 20 Linked Lists (en for hver rad). Dette gjorde det enkelt å implementere navigasjon til pekeren og inserting av data i listene. Selve navigeringen oppfører seg i stor grad likt som i en 2d array som eksisterte i det vi begynte med oppgaven. 
+
+I Document er det lagt til følgende funksjoner: innsetting, linjeskift, navigering, sletting og forskyvning av tekst om pekeren blir flyttet bakover. I tilleg har det blitt implementert logikk for å behandle outOfBoundsExceptions. Dette er tross alt det minste man forventer av en moderne teksteditor. 
+
